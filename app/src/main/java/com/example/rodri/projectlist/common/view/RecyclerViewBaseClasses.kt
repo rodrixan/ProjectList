f3acff45134304b8bbba9d9001b570690c4c4487
Rodri de Blas <rodri.xan@gmail.com>
2018-10-13 17:20:58 +0200

    Added diffutil for item update. Using async aprroach

--- app/src/main/java/com/example/rodri/projectlist/common/view/RecyclerViewBaseClasses.kt
+++ app/src/main/java/com/example/rodri/projectlist/common/view/RecyclerViewBaseClasses.kt
@@ -2,7 +2,11 @@ package com.example.rodri.projectlist.common.view
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.util.*
 
 data class ViewWrapper<V : View>(val view: V) : RecyclerView.ViewHolder(view)
 
@@ -13,15 +17,41 @@ interface RecyclerViewBaseBinder<T, V : View> {
 abstract class RecyclerViewBaseAdapter<T, V : View> : RecyclerView.Adapter<ViewWrapper<V>>(),
     RecyclerViewBaseBinder<T, V> {
 
+    private val updateQueue: Queue<List<T>> = ArrayDeque()
+
     protected var items: MutableList<T> = ArrayList()
-        set(value) {
-            if (items.isEmpty()) {
-                items = ArrayList(value)
-                notifyItemRangeInserted(0, value.size)
-            } else {
-                //DiffUtil
+        private set
+
+    fun updateItems(newItems: List<T>) {
+        updateQueue.add(newItems)
+        if (updateQueue.size > 1) {
+            return
+        }
+        updateItemsAsync(newItems)
+    }
+
+    private fun updateItemsAsync(new: List<T>) {
+        doAsync {
+            val diffResult = DiffUtil.calculateDiff(BaseAdapterDiffCallback(ArrayList(items), new))
+            uiThread {
+                applyUpdates(diffResult, new)
             }
         }
+    }
+
+    private fun applyUpdates(result: DiffUtil.DiffResult, new: List<T>) {
+        updateQueue.remove(new)
+        dispatchUpdates(result, new)
+        if (updateQueue.isNotEmpty()) {
+            updateItemsAsync(updateQueue.peek())
+        }
+    }
+
+    private fun dispatchUpdates(result: DiffUtil.DiffResult, new: List<T>) {
+        result.dispatchUpdatesTo(this)
+        items.clear()
+        items.addAll(new)
+    }
 
     protected abstract fun onCreateItemView(parent: ViewGroup, viewType: Int): V
 
@@ -34,4 +64,18 @@ abstract class RecyclerViewBaseAdapter<T, V : View> : RecyclerView.Adapter<ViewW
         bind(items[position], holder.view)
     }
 
+}
+
+class BaseAdapterDiffCallback<T>(private val oldList: List<T>, private val newList: List<T>) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int = oldList.size
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList[oldItemPosition]?.let {
+            it!!.equals(newList[newItemPosition])
+        } ?: false
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        areItemsTheSame(oldItemPosition, newItemPosition)
 }
