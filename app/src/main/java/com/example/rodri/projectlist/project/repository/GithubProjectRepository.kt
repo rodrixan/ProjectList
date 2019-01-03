b500105bfedaf91e8f8b757f9ed62748053d2f4c
Rodri de Blas <rodri.xan@gmail.com>
2019-01-03 12:15:05 +0100

    Added some images. Reordered code

--- app/src/main/java/com/example/rodri/projectlist/project/repository/GithubProjectRepository.kt
+++ app/src/main/java/com/example/rodri/projectlist/project/repository/GithubProjectRepository.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.rodri.projectlist.common.data.AppInternalData
 import com.example.rodri.projectlist.common.rest.model.ProjectListItem
+import com.example.rodri.projectlist.common.rest.model.User
 import com.example.rodri.projectlist.common.rest.service.ProjectApiService
 import com.example.rodri.projectlist.common.rest.service.awaitForResult
 import kotlinx.coroutines.Dispatchers
@@ -11,10 +12,18 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import timber.log.Timber
+import java.net.HttpURLConnection
 
 class GithubProjectRepository : ProjectRepository {
 
     val projectListData = MutableLiveData<AppInternalData<List<ProjectListItem>>>()
+
+    val dummyList = listOf(
+        ProjectListItem(0, "Test1", User(), 1, "Java"),
+        ProjectListItem(1, "Test2", User(), 4, "Python"),
+        ProjectListItem(0, "Test3", User(), 20, "C++"),
+        ProjectListItem(0, "Test3", User(), 20, "JavaScript")
+    )
 
     override fun getProjectList(userId: String): LiveData<AppInternalData<List<ProjectListItem>>> {
 
@@ -30,7 +39,11 @@ class GithubProjectRepository : ProjectRepository {
                         .awaitForResult { ProjectApiService.serviceResponseToAppError(it) }
                 Timber.d("Received response: success? ${apiResponse is AppInternalData.Success}")
                 withContext(Dispatchers.Main) {
-                    projectListData.value = apiResponse
+                    projectListData.value = if(apiResponse is AppInternalData.Error && apiResponse.code==HttpURLConnection.HTTP_FORBIDDEN){
+                        AppInternalData.Success(dummyList)
+                    }else {
+                        apiResponse
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/rodri/projectlist/project/view/adapter/ProjectAdapterClasses.kt
+++ app/src/main/java/com/example/rodri/projectlist/project/view/adapter/ProjectAdapterClasses.kt
@@ -1,8 +1,12 @@
 package com.example.rodri.projectlist.project.view.adapter
 
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.rodri.projectlist.R
 import com.example.rodri.projectlist.common.rest.model.ProjectListItem
 import com.example.rodri.projectlist.common.view.RecyclerViewBaseAdapter
@@ -14,7 +18,20 @@ class ProjectAdapter : RecyclerViewBaseAdapter<ProjectListItem, ProjectListItemV
         view.projectLanguage.text = view.context.getString(R.string.rv_item_project_language_placeholder, data.language)
         view.projectWatchers.text =
                 view.context.getString(R.string.rv_item_project_watchers_placeholder, data.watchersCount)
+        view.projectAvatar.loadDrawable(getIconByLanguage(data.language))
     }
+
+    fun getIconByLanguage(lang: String?) =
+        when (lang) {
+            "Java" -> R.mipmap.ic_java
+            "C" -> R.mipmap.ic_c_lang
+            "C++" -> R.mipmap.ic_cpp
+            "JavaScript" -> R.mipmap.ic_js
+            "Python" -> R.mipmap.ic_python
+            null -> -1
+            else -> -1
+        }
+
 
     override fun onCreateItemView(parent: ViewGroup, viewType: Int): ProjectListItemView =
         ProjectListItemView(parent, viewType)
@@ -25,6 +42,7 @@ class ProjectListItemView(parent: ViewGroup, viewType: Int) : ConstraintLayout(p
     var projectName: TextView
     var projectLanguage: TextView
     var projectWatchers: TextView
+    var projectAvatar: ImageView
 
     init {
         inflate(context, R.layout.rv_item_project, this)
@@ -32,5 +50,13 @@ class ProjectListItemView(parent: ViewGroup, viewType: Int) : ConstraintLayout(p
         projectName = findViewById(R.id.projectName)
         projectLanguage = findViewById(R.id.projectLanguage)
         projectWatchers = findViewById(R.id.projectWatchers)
+        projectAvatar = findViewById(R.id.projectAvatar)
+
+    }
+}
+
+fun ImageView.loadDrawable(drawableId:Int){
+    if(drawableId!=-1) {
+        Glide.with(context.applicationContext).load(drawableId).apply(RequestOptions().fitCenter()).into(this)
     }
 }
